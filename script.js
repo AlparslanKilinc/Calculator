b4047c398137ea9c8d77dacca206fa204688d3af
Alparslan Kilinc <kilincalparslan70@gmail.com>
2022-05-21 19:18:17 -0400

    initial style set up

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('Hello Script');
+
+
+// Basic Math 
+const add = (a,b) => a+b;
+const subtract = (a,b) => a-b;
+const multiply = (a,b) => a*b;
+const divide = (a,b) => a/b;
+
+const operate = (a,b,operator) =>{
+    switch(operator){
+        case '+': add(a,b);
+        case '-': subtract(a,b);
+        case '*': multiply(a,b);
+        case '/': divide(a,b);
+    }
+};
