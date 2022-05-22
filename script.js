d0a9b2ad76ddd654044542280f060ea6feccabcb
Alparslan Kilinc <kilincalparslan70@gmail.com>
2022-05-22 16:00:30 -0400

    add calculation

--- script.js
+++ script.js
@@ -1,9 +1,68 @@
+/// Variables 
+let firstOperand = '';
+let secondOperand = '';
+let currentOperation = null;
+let hold ='';
+
+
 
 /// Get Elements 
-    const calculation = document.querySelector('.calculation');
-    const result = document.querySelector('.result');
-    let c = 0;
-    let d = 0;
+    const LastDisplay = document.getElementById('Last-Operation');
+    const CurrentDisplay = document.getElementById('Current-Operation');
+    const dotButton = document.getElementById('dot');
+    const equalsButton = document.getElementById('equals');
+    const clearButton = document.getElementById('clear');
+    const delButton = document.getElementById('delete');
+    const numberButtons = document.querySelectorAll('[data-number]');
+    const operatorButtons = document.querySelectorAll('[data-operation]');
+    
+
+/// Event Handling
+    // equalsButton.addEventListener('click', evaluate);
+    // clearButton.addEventListener('click', clear);
+    // delButton.addEventListener('click', deleteNumber);
+    // dotButton.addEventListener('click', appendPoint);
+
+/// Appending number to CurrentDisplay
+
+    numberButtons.forEach( (num) => num.addEventListener('click', () => appendNumber(num.textContent)));
+    function appendNumber(input){
+        LastDisplay.textContent += input;
+        hold += input;
+    }
+
+    operatorButtons.forEach( (operator) => operator.addEventListener('click', () =>{
+            if(currentOperation!==null ){
+            calculate();
+            currentOperation=operator.textContent;
+            LastDisplay.textContent += operator.textContent;
+            }
+
+            else 
+            {
+                LastDisplay.textContent += operator.textContent;
+                firstOperand = hold;
+                currentOperation = operator.textContent;
+                hold='';
+               
+            }
+            
+           
+    }));
+
+//// Functions 
+    function calculate(){
+       secondOperand = hold;
+       let result =operate(firstOperand,secondOperand,currentOperation);
+       console.log(result);
+       CurrentDisplay.textContent = `${result}`;
+       hold='';
+       firstOperand=result;
+       LastDisplay.textContent=`${result}`;
+    }
+    
+
+
 
 // Basic Math 
 const add = (a,b) => a+b;
@@ -11,103 +70,29 @@ const subtract = (a,b) => a-b;
 const multiply = (a,b) => a*b;
 const divide = (a,b) => a/b;
 
-/// clear 
-
-function clears (){ 
-    calculation.textContent='';
-    console.log('heyy');
-}
-
 const operate = (a,b,operator) =>{
+    a=Number(a);
+    b=Number(b);
     switch(operator){
-        case '+': result.textContent=add(a,b);
-        case '-': result.textContent=subtract(a,b);
-        case '*': result.textContent=multiply(a,b);
-        case '/': result.textContent=divide(a,b);
+
+        case '+':
+        return add(a,b);
+       
+        case '-': 
+        return subtract(a,b);
+       
+        case 'x': 
+        return multiply(a,b);
+      
+        case '/':
+        if(b==='0') return null;
+        else return divide(a,b);
+
+        default:
+        return null;
     }
 };
 
-
-
-function append(input){
-
-            switch(input) 
-            {
-                case '1': 
-                calculation.textContent += '1';
-                break;
-
-                case '2': 
-                calculation.textContent += '2';
-                break;
-
-                case '3': 
-                calculation.textContent += '3';
-                break;
-
-                case '4': 
-                calculation.textContent += '4';
-                break;
-
-                case '5': 
-                calculation.textContent += '5';
-                break;
-
-                case '6': 
-                calculation.textContent += '6';
-                break;
-
-                case '7': 
-                calculation.textContent += '7';
-                break;
-
-                case '8': 
-                calculation.textContent += '8';
-                break;
-
-                case '9': 
-                calculation.textContent += '9';
-                break;
-
-                case '0': 
-                calculation.textContent += '0';
-                break;
-    
-                case '+': 
-                if(calculation.textContent.charAt(calculation.textContent.length-1)==='+') break;
-                calculation.textContent += '+';
-                break;
-
-                case '-': 
-                if(calculation.textContent.charAt(calculation.textContent.length-1)==='-') break;
-                calculation.textContent += '-';
-                break;
-
-                case 'x': 
-                if(calculation.textContent.charAt(calculation.textContent.length-1)==='x') break;
-                calculation.textContent += 'x';
-                break;
-
-                case '/': 
-                if(calculation.textContent.charAt(calculation.textContent.length-1)==='/') break;
-                calculation.textContent += '/';
-                break;
-
-                case '.':
-                if(calculation.textContent.charAt(calculation.textContent.length-1)==='.') break;
-                calculation.textContent += '.';
-                break;
-    
-                case '=':
-                const array = calculation.textContent.split('+');
-                if(calculation.textContent.includes('+')) operate(c,d,add);
-                else if(calculation.textContent.includes('-')) operate(c,d,subtract);
-                else if(calculation.textContent.includes('*')) operate(c,d,multiply);
-                else if(calculation.textContent.includes('/')) operate(c,d,divide);
-                break;
-
-            }
-    }
 
 
 
